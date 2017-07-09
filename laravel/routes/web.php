75ada0f61d4e5e555a7f499ca7df483928acfc68
dragon <zhangjunlong_927@163.com>
2017-07-09 19:00:12 +0800

    代码优化

---
+++ laravel/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo 'I have a dream';
+});
+
+Route::get('index/index', 'Admin\IndexController@index');
+
+Route::post('/form', 'Admin\IndexController@form');
+
+Route::get('index/add', 'Admin\IndexController@index');
+
+Route::get('test/test', 'Admin\TestController@test');
+
+Route::any('/login/login', 'Admin\LoginController@login');

6e2e7c67dbe1241a1f98e9a8bcc6f0a2ad34e7b8
dragon <zhangjunlong_927@163.com>
2017-07-09 18:49:31 +0800

    代码优化

---
+++ laravel/app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Model\UserModel;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    //登录
+    public function login(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+            $rules = ['captcha' => 'required|captcha'];
+            $message = [
+                'captcha.required' => '验证码必须填写',
+                'captcha.captcha' => '验证码输入错误'
+            ];
+            $validator = Validator::make($data, $rules, $message);
+            if ($validator->fails()) {
+                //back() 表示返回上一界面
+                return back()->withErrors($validator);
+            } else {
+                //验证码正确进行下面的验证
+                //调用模型层验证
+                $info = UserModel::login($data['name'], $data['password']);
+                if ($info === true) {
+                    return redirect()->to('/');
+                } else {
+                    return back()->with('msg', $info);
+                }
+            }
+        }
+        //加载登录页面
+        return view('admin.login');
+    }
+}
